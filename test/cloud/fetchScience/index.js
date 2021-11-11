ef4077ed02ab800a46a7e569ae52beb0ce0548b2
JiayiNie <86969929+JiayiNie@users.noreply.github.com>
2021-11-11 00:50:08 +0000

    cloud function APIs and wechat official APIs

---
+++ test/cloud/fetchScience/index.js
@@ -0,0 +1,18 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env:"qjjh-0gycpoief2f304db"
+})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return cloud.database().collection("science").get({
+    success(res){
+      return res
+    },
+    fail(err){
+      return err
+    }
+  })
+}
